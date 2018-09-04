b7b3d457e90d7392c7839e5daa36e8ecc1933ad7
yihuan18 <18200120142@163.com>
2018-09-04 20:54:14 +0800

    origin

---
+++ communicate.h
@@ -0,0 +1,12 @@
+#include <sys/socket.h>
+#include <string.h>
+#include <linux/in.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PORT 5000
+
+int csocfd;
+struct sockaddr_in mysockaddr;
+u8* seed_buf;
